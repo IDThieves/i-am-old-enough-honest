19255e84d83df357f87971b004c26f355a9089a7
sarah <sarahabimay@gmail.com>
2015-04-27 14:07:15 +0100

    initial file structure

---
+++ api/controllers.js
@@ -0,0 +1,86 @@
+var Bell 	= require('bell');
+var Path 	= require('path');
+// var Joi 	= require('joi');
+var opentok = require('./opentok');
+var members = require('./models/members.js');
+
+
+var config 	= require('./config');
+
+/////////////
+// Helpers //
+/////////////
+
+/////////////
+// Handlers//
+/////////////
+
+module.exports = {
+
+	serveFile: {
+		auth: false,
+		handler: {
+			directory: {
+				path: '../public'
+			}
+		}
+	},
+
+	loginFacebook: {
+		 auth: {
+			strategy: 'facebook'
+		 },
+		 handler: function (request, reply) {
+			if (request.auth.isAuthenticated) {
+				var fb = request.auth.credentials;
+				console.dir(fb);
+				var username = fb.profile.displayName || fb.profile.email.replace(/[^\w]/g,'') + (Math.random()*100).toFixed(0);
+				var profile = {
+					username 	: username,
+					email 		: fb.profile.email,
+					error 		: null
+				};
+				console.log('Profile:');
+				console.dir(profile);
+				return reply( JSON.stringigy( profile ) );
+			}
+			else {
+				return reply.redirect('/loggedout');
+			}
+		}
+	},
+
+	logout: {
+		handler: function (request, reply){
+			request.auth.session.clear();
+			return reply.redirect('/loggedout');
+		}
+	},
+
+	loggedoutView: {
+		auth: false,
+		handler: function (request, reply) {
+			return reply('Logged out');
+		}
+	},
+
+	homeView: {
+		handler: function (request, reply ){
+			if (request.auth.isAuthenticated) {
+				return reply( 'Upload or View your Id.' );
+			}
+			else {
+				console.log( 'You are not authorised');
+				return reply('You are not an authorised user.');
+			}
+		}
+	},
+
+	// api routes:
+	imageUpload  : {
+		handler: function( request, reply ) {
+			return reply( 'Upload Image Request received.');
+		}
+
+	}
+};
---
+++ app.js
@@ -0,0 +1,20 @@
+var server 		= require('./api/server');
+
+var mongoose 	= require("mongoose");
+var config 		= require("./api/config").db;
+var mongodbUri 	= config.dburl;
+
+server.start(function () {
+
+	mongoose.connect(mongodbUri, function() {
+		var db = mongoose.connection;
+
+		db.on("error", console.error.bind(console, "connection error"));
+		db.once("open", function() {
+			console.log("database connection successful");
+		});
+	});
+
+	console.log('Server running at:', server.info.uri);
+});
+
