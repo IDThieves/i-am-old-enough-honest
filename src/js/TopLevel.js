ee7de3762500a3445858ff025d0e7ea0bb76a152
Anita Amini <baadge29@yahoo.co.uk>
2015-04-27 15:16:44 +0100

    flux file system setup

---
+++ src/js/TopLevel.js
@@ -0,0 +1,6 @@
+var React 	 = require("react");
+var KiwiApp = require("./components/KiwiApp");
+ 
+React.render(
+	<KiwiApp />, document.getElementById("content")
+	);
---
+++ src/js/constants/Constants.js
@@ -0,0 +1,13 @@
+var keyMirror = require("react/lib/keyMirror");
+
+module.exports = {
+    
+    ActionTypes: keyMirror({
+        
+        NAVIGATE_TO: null,
+        
+        //Server
+        RECEIVE_MEMBER: null
+
+    })
+};
