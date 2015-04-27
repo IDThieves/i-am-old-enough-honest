ebee2ca10cc01427abb1bca7baa6a805b421764e
Anita Amini <baadge29@yahoo.co.uk>
2015-04-27 15:05:20 +0100

    schema stuff

--- api/models/schema.js
+++ api/models/schema.js
@@ -1,1 +1,19 @@
-schema.js
+var mongoose 	= require('mongoose');
+var Schema 		= mongoose.Schema;
+
+
+var userSchema = new Schema({
+	username: 	{type: String, required: true},
+	email: 		{type: String, required: true},
+	firstName: 	{type: String, required: true},
+	lastName: 	{type: String, required: true},
+	signUpDate: {type: Date, required: true},
+	approved: 	{type: Boolean, required: true}
+});
+
+
+var User = mongoose.model('user', userSchema);
+
+module.exports = {
+	User: User
+};
