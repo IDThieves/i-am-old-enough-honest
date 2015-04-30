6cf6217767204e21fa0c4a1558db3a769e088cb2
amilvasishtha <amil_v@msn.com>
2015-04-30 13:25:16 +0100

    working on the upload.js file

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -11,3 +11,20 @@ function loadImg(input) {
 		reader.readAsDataURL(input.files[0]);
 	}
 }
+
+$('#submitID').on('click', function() {
+	var usernameTag = $(".username");
+	console.log(usernameTag);
+	console.log(usernameTag["0"].innerHTML);
+	var username = usernameTag["0"].innerHTML;
+	var imageId = $("input[name='uploadedIDname']");
+	console.log(imageId["0"].value);
+	// console.log("imageId:", imageId);
+
+	var payload = {
+		username: username,
+
+	};
+
+	$.post();
+});
