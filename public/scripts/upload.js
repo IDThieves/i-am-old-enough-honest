4ef6a37f02f150e0c4b72e85825bcf4033d446ff
Anita Amini <baadge29@yahoo.co.uk>
2015-05-05 19:10:52 +0100

    cleaning up a bit

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -14,38 +14,21 @@ function loadImg(input) {
 }
 
 //sending an AJAX to our API
-//$('#submitID').on('click', function() {
-//	var usernameTag = $(".username");
-//	console.log(usernameTag);
-//	console.log(usernameTag.text());
-////	var username = usernameTag.text();
-//	var image = $("input[name='uploadedIDname']");
-//	var IDImage = image.val();
-//
-//	var payload = {
-////		username: username,
-//		IDImage: IDImage
-//	};
-//
-//	$.post("/api/image", {data: payload}, function(result){
-//		console.log("result:",result);
-//	});
-//});
+$('#submitID').on('click', function() {
+	var usernameTag = $(".username");
+	console.log(usernameTag);
+	console.log(usernameTag.text());
+	var username = usernameTag.text();
+	var image = $("input[name='uploadedIDname']");
+	var IDImage = image.val();
 
+	var payload = {
+		username: username,
+		IDImage: IDImage
+	};
 
-function upload() {
-var file = document.getElementById('uploadedID').files[0];
-var fd = new FormData();
-var key = file.name;
-    fd.append('key', key);
-    fd.append("file",file);
+	$.post("/api/image", {data: payload}, function(result){
+		console.log("result:",result);
+	});
+});
 
-    var xhr = new XMLHttpRequest();
-
-    xhr.upload.addEventListener("progress", uploadProgress, false);
-
-    xhr.open('POST', 'https://kiwi-idimage.s3.amazonaws.com/', true);
-    xhr.send(fd);
-};
-
-upload();
